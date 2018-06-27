70cdf84d328de688d660369044729c9b41e90383
nyamed <nyamedavis@gmail.com>
2018-06-27 00:30:49 -0400

    website started
    created homepage.php
    header.php
    style.css

---
+++ homepage.php
@@ -0,0 +1,51 @@
+<html lang="en">
+<head>
+  <title>Book It n'Come</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+  <h1> </h1>
+  <div id="templatemo_header">
+    	<div id="site_title"><h1><a href="#">Book It n'Come_Events</a></h1></div>
+        <div id="header_right">
+        	<p>
+	        <a href="#">My Account</a> | <a href="#">My Wishlist</a> | <a href="#">My Cart</a> | <a href="#">Checkout</a> | <a href="#">Log In</a></p>
+            <p>
+            	Shopping Cart: <strong>0 items</strong> ( <a href="shoppingcart.html">Show Cart</a> )
+			</p>
+		</div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_header -->
+	
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Book It n'Come</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Events<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">About Us</a></li>
+		
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+</body>
+
+</html>
+
