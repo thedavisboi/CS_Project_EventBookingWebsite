2e9e91b32deffadcecbbe04ff50faba029da63b2
nyamed <nyamedavis@gmail.com>
2018-08-08 01:24:42 -0400

    Amended carousel using bootstrap

--- index.php
+++ index.php
@@ -1,26 +1,55 @@
 <?php include "header.php" ?>
 
-<div id="slider-wrapper">
-    <div id="slider" class="nivoSlider">
-        <img src="images/slider/02.jpg" alt="" />
-            <a href="#"><img src="images/slider/01.jpg" alt="" title="This is an example of a caption" /></a>
-                <img src="images/slider/03.jpg" alt="" />
-                    <img src="images/slider/04.jpg" alt="" title="#htmlcaption" />
-    </div>
-    <div id="htmlcaption" class="nivo-html-caption">
-        <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
-    </div>
-</div>
-<script type="text/javascript" src="js/jquery-1.4.3.min.js"></script>
-            <script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
-            <script type="text/javascript">
-            $(window).load(function() {
-                $('#slider').nivoSlider();
-            });
-</script>
-
 <div class="container">
   Inside!!!
+  <h2>Carousel Example</h2>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="images/slider/01.jpg" alt="Los Angeles" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Los Angeles</h3>
+          <p>LA is always so much fun!</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="images/slider/02.jpg" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago!</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="images/slider/03.jpg" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>We love the Big Apple!</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 </div>
 
 <?php include "footer.php" ?>
