b085ae94b5f419f04794e65e39dea32e9e4d4e56
nyamed <nyamedavis@gmail.com>
2018-08-14 15:11:51 -0400

    created register form in registration.php & linked index.php

--- header.php
+++ header.php
@@ -17,10 +17,10 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Book It 'n Come</a>
+      <a class="navbar-brand" href="index.php">Book It 'n Come</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
+      <li class="active"><a href="index.php">Home</a></li>
 	  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Events<span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">Current Events</a></li>
@@ -32,7 +32,7 @@
       <li><a href="#">Page 2</a></li>
 	</ul>
 	<ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
       <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
 	<form class="navbar-form navbar-right" action="/action_page.php">
